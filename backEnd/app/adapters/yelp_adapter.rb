38a15929685950c4572c450a385f0fbe667a67a3
James Segarra III <JamesSegarra@gmail.com>
2016-12-02 14:42:08 -0500

    yelp progress

---
+++ backEnd/app/adapters/yelp_adapter.rb
@@ -0,0 +1,17 @@
+class YelpApi
+
+  def self.client
+
+    @client ||= Yelp::Client.new({ consumer_key: "VgBqgM2m0cFktXaWg7MirA",
+                            consumer_secret: "CwavGnQCgdvdVY9fWELZ3ngWhjc",
+                            token: "6ekU32QlnASLZQ29K6jV2iH8jg9M39ap",
+                            token_secret: "txCmSP_tTYILR2nNPXtJq30RhCQ"
+                          })
+
+  end
+
+  def self.search(search:, location:)
+    businesses = self.client.search(location, {term: search,radius_filter:1000}).businesses
+  end
+
+end

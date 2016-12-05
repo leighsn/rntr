ea601d72df3eaf286144af2a2d1097181dcbb437
Craig Iturbe <citurbe@gmail.com>
2016-12-05 09:51:06 -0500

    small change

--- backEnd/app/controllers/apts_controller.rb
+++ backEnd/app/controllers/apts_controller.rb
@@ -16,17 +16,57 @@ class AptsController < ApplicationController
   end
 
   private
-  def commute_score
+  def commute_score(duration)
+    if duration < 10
+      score = 10
+    elsif duration >= 10 && duration < 15
+      score = 9
+    elsif duration >= 15 && duration < 20
+      score = 8
+    elsif duration >= 20 && duration < 25
+      score = 7
+    elsif duration >= 25 && duration < 30
+      score = 6
+    elsif duration >= 30 && duration < 35
+      score = 5
+    elsif duration >= 35 && duration < 40
+      score = 4
+    elsif duration >= 40 && duration < 45
+      score = 3
+    elsif duration >= 45 && duration < 50
+      score = 2
+    else
+      score = 1
+
   end
 
   def crime_score(felonies)
+    score = felonies / 10
+    score >= 10 ? roundScore = 9 : roundScore = score.floor
+    10 - roundScore
 
   end
 
   def amenities_score
+    7
   end
 
-  def school_score
+  def school_score(schools)
+    schools > 10 ? score = 10 : score = schools
+    score
+  end
+
+  def calculate_weights(prefs)
+    total = prefs[:schools] + prefs[:amenities] + prefs[:safety] + prefs[:commute]
+    weights[:schools] = prefs[:schools] / total * 10
+    weights[:amenities] = prefs[:amenities] / total * 10
+    weights[:safety] = prefs[:safety] / total * 10
+    weights[:commute] = prefs[:commute] / total * 10
+    weights
+  end
+
+  def calculate_score(scores, weights)
+    (scores[:schools] * weights[:schools]) + (scores[:commute] * weights[:commute]) + (scores[:safety] * weights[:safety]) + (scores[:amenities] * weights[:amenities])
   end
 
 end

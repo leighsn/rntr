f616e2fa8de7367bc9819c2939b1e398577d3a70
Craig Iturbe <citurbe@gmail.com>
2016-12-07 18:08:43 -0500

    fix one of jeffs requests

--- frontEnd/src/components/SignUpComponent.js
+++ frontEnd/src/components/SignUpComponent.js
@@ -4,42 +4,66 @@ import { signUp } from '../actions/action.js'
 import { bindActionCreators } from 'redux';
 import { browserHistory } from 'react-router'
 
+class SignUpComponent extends React.Component {
+
+   constructor(props) {
+    super(props)
+    this.state = {email: "", password: "", confirmation: ""}
+  }
 
 
-const SignUpComponent = (props) => {
-
-  function handleSubmit(event){
+   handleSubmit(event){
     event.preventDefault()
-    props.signUp(event.target.children[1].value, event.target.children[4].value)
+    if (this.state.password !== this.state.confirmation){
+      alert('Password and Confirmation do not Match!')
+      browserHistory.push('sign-up')
+    }
+    else {
+      this.props.signUp(this.state.email, this.state.password)
+    }
+    // props.signUp(event.target.children[1].value, event.target.children[4].value)
     // this.setState({..})
   }
 
+   handleEmailChange(event){
+    this.setState({email: event.target.value})
+  }
 
-  function handleCreateAccountLink(event){
+   handlePasswordChange(event){
+    this.setState({password: event.target.value})
+  }
+
+   handleConfirmChange(event){
+    this.setState({confirmation: event.target.value})
+  }
+
+
+   handleCreateAccountLink(event){
     event.preventDefault()
     browserHistory.push('log-in')
   }
-
+  render(){
   return (
     <div id='padding'>
     <center>
-      <form onSubmit={handleSubmit.bind(this)}>
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <label>Email:</label>
-        <input type='email' id='email' /><br/>
+        <input type='email' id='email' onChange={this.handleEmailChange.bind(this)} /><br/>
 
         <label>Password:</label>
-        <input type='password' id='password' /><br/>
+        <input type='password' id='password' onChange={this.handlePasswordChange.bind(this)} /><br/>
 
         <label>Confirm Password:</label>
-        <input type='password' id='confirm' /><br/>
+        <input type='password' id='confirm' onChange={this.handleConfirmChange.bind(this)} /><br/>
 
         <input type='submit' value='Create an Account' />
-        <p>or <a href="javascript:" onClick={handleCreateAccountLink.bind(this)}>Log In</a></p>
+        <p>or <a href="javascript:" onClick={this.handleCreateAccountLink.bind(this)}>Log In</a></p>
 
       </form>
       </center>
     </div>
   )
+}
 }
 
 

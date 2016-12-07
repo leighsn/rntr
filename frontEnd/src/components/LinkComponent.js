05be02e0ccec97ac487f59b724e93d80602a5d86
krissasantos <krissa.santos31@gmail.com>
2016-12-07 12:00:45 -0500

    made a dynamic navbar depending on whether or not a jwt exists in localStorage

--- frontEnd/src/components/LinkComponent.js
+++ frontEnd/src/components/LinkComponent.js
@@ -2,16 +2,30 @@ import React from 'react'
 import { Link } from 'react-router'
 
 const LinkComponent = () => {
+
+  // var navbar= "
+    // <li><Link to='/search'>Search</Link></li>
+    // 
+    // <li><Link to={"/sign-up"}>Sign Up</Link></li>
+    // "
+    // || localStorage.getItem('jwt')!== undefined
+  
+  var navbar
+  if (localStorage.getItem('jwt')!== "" ){ 
+    navbar=<ul id='nav'><li><Link to='/search'>Search</Link></li><li><Link to='/preferences'>Preferences</Link></li><li><Link to='/log-out'>Log Out</Link></li></ul>
+  
+  }
+  else {
+    navbar=<ul id='nav'><li><Link to='/log-in'>Log In</Link></li><li><Link to='/sign-up'>Sign Up</Link></li></ul>
+
+  }
+
   return (
-    <div>
-      <ul id='nav'>
-      <li><Link to="/search">Search</Link></li>
-      <li><Link to="/preferences">Preferences</Link></li>
-      <li><Link to="/sign-up">Sign Up</Link></li>
-      <li><Link to="/log-in">Log In</Link></li>
-      </ul>
-    </div>
+    <div>{navbar}</div>
   )
 }
 
 module.exports = LinkComponent
+
+
+

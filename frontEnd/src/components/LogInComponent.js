1f07c64930e6b6ab9eeac638ff68c880579373cc
Craig Iturbe <citurbe@gmail.com>
2016-12-08 09:19:04 -0500

    stop getting values from the DOM

--- frontEnd/src/components/LogInComponent.js
+++ frontEnd/src/components/LogInComponent.js
@@ -5,40 +5,55 @@ import { bindActionCreators } from 'redux';
 import { browserHistory } from 'react-router'
 
 
-const LogInComponent = (props) => {
+class LogInComponent extends React.Component {
 
-  function handleSubmit(event){
-    event.preventDefault()
-    props.logIn(event.target.children[1].value, event.target.children[4].value)
+  constructor(props){
+    super(props)
+    this.state = {email: "", password: ""}
   }
 
- function handleCreateAccountLink(event){
+
+   handleSubmit(event){
+    event.preventDefault()
+    this.props.logIn(this.state.email, this.state.password)
+  }
+
+  handleCreateAccountLink(event){
     event.preventDefault()
     browserHistory.push('sign-up')
   }
 
+   handleEmailChange(event){
+     this.setState({email: event.target.value})
+  }
 
+  handlePasswordChange(event){
+    this.setState({password: event.target.value})
+ }
+
+  render(){
   return (
     <div id='padding'>
-   
+
     <center>
-      <form  onSubmit={handleSubmit.bind(this)}>
-       
+      <form  onSubmit={this.handleSubmit.bind(this)}>
+
         <label>Email:</label>
-        <input type='email' id='email' /><br/>
+        <input onChange={this.handleEmailChange.bind(this)}type='email' id='email' /><br/>
 
         <label>Password:</label>
-        <input type='password' id='password' /><br/>
+        <input onChange={this.handlePasswordChange.bind(this)} type='password' id='password' /><br/>
 
-        <input type='submit' value='log in' /> 
+        <input type='submit' value='log in' />
         <hr width='20%'id='hr'/>
 
-        <a href="javascript:" onClick={handleCreateAccountLink.bind(this)}> Create an Account</a>
-        
+        <a href="javascript:" onClick={this.handleCreateAccountLink.bind(this)}> Create an Account</a>
+
       </form>
   </center>
     </div>
   )
+}
 }
 
 

d79596e7e3beb8a0dd09df44c5b903998fc1a3a6
krissasantos <krissa.santos31@gmail.com>
2016-12-08 18:12:15 -0500

    every gauge appears on screen for each preference

--- frontEnd/src/components/ResultsComponent.js
+++ frontEnd/src/components/ResultsComponent.js
@@ -64,8 +64,6 @@ class ResultsComponent extends Component {
       commute_gauge.setValueAnimated(this.props.user.commute, 1);
 
 
-
-        
       let schools_gauge = Gauge(
       document.getElementById("schools_gauge"), {
         max: 10,
@@ -76,6 +74,17 @@ class ResultsComponent extends Component {
           return Math.round(value) + " / " + this.max;
         }})
       schools_gauge.setValueAnimated(this.props.user.schools, 1);
+
+      let amenities_gauge = Gauge(
+      document.getElementById("amenities_gauge"), {
+        max: 10,
+        dialStartAngle: -90,
+        dialEndAngle: -90.001,
+        value: 0,
+        label: function(value) {
+          return Math.round(value) + " / " + this.max;
+        }})
+      amenities_gauge.setValueAnimated(this.props.user.amenities, 1);
   }
 
 
@@ -85,47 +94,46 @@ class ResultsComponent extends Component {
 
     return (
       <div>
-
         <div id="gauge1"></div>
+        <h4>{this.props.apartment.address}</h4>
 
-      <h4>{this.props.apartment.address}</h4>
-
-      <h2>Score: <DonutChart color='orange' value={this.props.apartment.apartment_score} size='116' strokewidth='26'/></h2>
+      <p>Within the past year, there were {this.props.apartment.data.crime_data.felonies} felonies, {this.props.apartment.data.crime_data.misdemeanors} misdemeanors, and {this.props.apartment.data.crime_data.violations} violations reported near the apartment.
       <p>The commute time from this apartment to your workplace is {this.props.apartment.data.distance_data} mins.</p>
-      <p>Within the past year, there were {this.props.apartment.data.crime_data.felonies} felonies, {this.props.apartment.data.crime_data.misdemeanors} misdemeanors, and {this.props.apartment.data.crime_data.violations} violations reported near the apartment.</p>
-      <ul>
-      <li>{this.props.apartment.data.school_data.a_schools} schools nearby were given a grade of 'A'</li>
-      <li>{this.props.apartment.data.school_data.b_schools} schools nearby were given a grade of 'B'</li>
-      <li>{this.props.apartment.data.school_data.c_schools} schools nearby were given a grade of 'C'</li>
-      <li>{this.props.apartment.data.school_data.d_schools} schools nearby were given a grade of 'D'</li>
-      <li>{this.props.apartment.data.school_data.a_schools} schools nearby were given a grade of 'F'</li>
-      </ul>
-      <p>Yelp found {this.props.apartment.data.amenities_data.category_1.count} locations that matched the search term "{this.props.apartment.data.amenities_data.category_1.name}"</p>
-      <p>Yelp found {this.props.apartment.data.amenities_data.category_2.count} locations that matched the search term "{this.props.apartment.data.amenities_data.category_2.name}"</p>
-      <p>Yelp found {this.props.apartment.data.amenities_data.category_3.count} locations that matched the search term "{this.props.apartment.data.amenities_data.category_3.name}"</p>
+
+        <ul>
+          <li>{this.props.apartment.data.school_data.a_schools} schools nearby were given a grade of 'A'</li>
+          <li>{this.props.apartment.data.school_data.b_schools} schools nearby were given a grade of 'B'</li>
+          <li>{this.props.apartment.data.school_data.c_schools} schools nearby were given a grade of 'C'</li>
+          <li>{this.props.apartment.data.school_data.d_schools} schools nearby were given a grade of 'D'</li>
+          <li>{this.props.apartment.data.school_data.a_schools} schools nearby were given a grade of 'F'</li>
+          <li>Yelp found {this.props.apartment.data.amenities_data.category_1.count} locations that matched the search term "{this.props.apartment.data.amenities_data.category_1.name}"</li>
+          <li>Yelp found {this.props.apartment.data.amenities_data.category_2.count} locations that matched the search term "{this.props.apartment.data.amenities_data.category_2.name}"</li>
+          <li>Yelp found {this.props.apartment.data.amenities_data.category_3.count} locations that matched the search term "{this.props.apartment.data.amenities_data.category_3.name}"</li>
+        </ul>
+      </p>
+
+
       <p>Your Preferences:</p>
-      <div className='row'>
-
-      <div className = 'two columns' id="commute_gauge">
-      <p>Commute</p>
-      </div>
-
-
-      <div className = 'two columns' id="safety_gauge">
-      <p>Safety</p>
-      </div>
+        <div className='row'>
+          <div className = 'two columns' id="commute_gauge">
+            <p>Commute</p>
+          </div>
 
 
-      <div className = 'two columns'id="schools_gauge"> 
-      <p>Schools</p>
-      </div>
-      <div className = 'two columns'>
-      <DonutChart value={this.props.user.amenities} size='80' strokewidth='16'/>
-      <p>Amenities</p>
-      </div>
 
-      </div>
+          <div className = 'two columns' id="safety_gauge">
+            <p>Safety</p>
+          </div>
 
+
+          <div className = 'two columns'id="schools_gauge"> 
+            <p>Schools</p>
+          </div>
+
+          <div className = 'two columns' id="amenities_gauge">
+            <p>Amenities</p>
+          </div>
+        </div>
       </div>
     )
   }

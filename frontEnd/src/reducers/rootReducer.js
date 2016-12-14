e2556577c44b81a07bccbc2b0e09e40e7a1160e2
Leigh Scherrer <leighscherrer@gmail.com>
2016-12-14 15:00:29 -0500

    Update rootReducer.js

--- frontEnd/src/reducers/rootReducer.js
+++ frontEnd/src/reducers/rootReducer.js
@@ -3,9 +3,20 @@ import { combineReducers } from 'redux'
 function user(state={}, action) {
   switch(action.type) {
     case 'SIGN_UP':
-      return {userID: action.payload.user_id, userEmail: action.payload.user_email}
+      return {userID: action.payload.user_id, 
+              userEmail: action.payload.user_email}
     case 'LOG_IN':
-      return {userID: action.payload.user_id, userEmail: action.payload.user_email, destination: action.payload.destination, commute: action.payload.commute, schools: action.payload.schools, safety: action.payload.safety, amenities: action.payload.amenities, category_1: action.payload.category_1, category_2: action.payload.category_2, category_3: action.payload.category_3}
+      //suggest fixing formatting for readability
+      return {userID: action.payload.user_id, 
+              userEmail: action.payload.user_email, 
+              destination: action.payload.destination, 
+              commute: action.payload.commute, 
+              schools: action.payload.schools, 
+              safety: action.payload.safety, 
+              amenities: action.payload.amenities, 
+              category_1: action.payload.category_1, 
+              category_2: action.payload.category_2, 
+              category_3: action.payload.category_3}
     case 'UPDATE_PREFERENCES':
     console.log(action.payload)
       return Object.assign({}, state,{userID: action.payload.user_id, destination: action.payload.destination, commute: action.payload.commute, schools: action.payload.schools, safety: action.payload.safety, amenities: action.payload.amenities})
